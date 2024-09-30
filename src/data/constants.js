2cf7584d291ec2c643c36c838cd8bf171ba9703e
dheeruz <132977881+dheeruz@users.noreply.github.com>
2024-09-30 12:50:30 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,272 @@
+export const Bio = {
+  name: "Sai Dheeraj A ",
+  roles: [
+    "Software Engineer",
+    "Android Developer",
+    "Python Developer",
+    "Programmer",
+  ],
+  description:
+"A Third-year Computer Science and Cybersecurity student, blending programming proficiency with a cybersecurity focus. Enthusiastic about crafting secure software solutions, actively engaging in hackathons and aspiring to innovate in the tech sphere. Committed to continuous learning for a future impactful role as a Software Engineer.",
+  github: "https://github.com/dheeruz",
+  resume:
+    "https://docs.google.com/document/d/1slXIuaWoHwJ9zVJVXpa6jNWR4SM6WBxLGG7cY74v6mA/edit",
+  linkedin: "https://www.linkedin.com/in/sai-dheeraj-a-78070226b/",
+  twitter: "https://x.com/Dheeraj20051876",
+  insta: "https://www.instagram.com/dheeru._.7/",
+  whatsapp: "https://api.whatsapp.com/send?phone=919100256717&text=%F0%9F%A5%B0%F0%9F%A5%B0%F0%9F%A5%B0",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+        "https://imgur.com/yuOsKKN.png"
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://imgur.com/tnFIV7n.png",
+      },
+      {
+        name: "Figma",
+        image:
+        "https://imgur.com/BN7wFaZ.png"
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://i.imgur.com/rXe1Kq3.png",
+    role: "Web Devlopment Intern",
+    company: "Aadhitya infomedia Technology",
+    date: "May 2023 - June 2023",
+    desc: "Worked on Customizing the Mission Planner which is used to control the UAVs.",
+    skills: [
+      "Html",
+      "Css",
+      "Visual Studio Code",
+      "Java Script",
+    ],
+    doc: "https://i.imgur.com/4euFFQ0.jpeg",
+  },
+  {
+    id: 1,
+    img: "https://i.imgur.com/swOM4hN.jpeg",
+    role: "Web Devlopment Intern",
+    company: "Internpe",
+    date: "July 2023 - Oct 2023",
+    desc: "Built an project plans like Recommendation Systems architecture using AWS.",
+    skills: [
+      "Html",
+      "Css",
+      "Visual Studio Code",
+      "Java Script",
+    ],
+    doc:"https://i.imgur.com/I6wmDaX.jpeg",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/9o57vcg.png",
+    role: "Bussiness Analyst Virtual Intern",
+    company: "Celonis",
+    date: "Nov 2023 - Jan 2024",
+    desc: "Developed an Android Application for the Company.",
+    skills:[
+      "Analytical Thinking",
+      "Data Analysis & Interpretation",
+      "Communication Skills",
+      "Problem-Solving",
+    ],
+    doc: "https://i.imgur.com/KVJYsmr.jpeg"
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://i.imgur.com/ZvqHBav.jpg",
+    school: "Chennai Institute of Technology,Chennai",
+    date: "Nov 2022 - May 2026",
+    grade: "8.01 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Chennai Institute of Technology,Chennai. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.I am currently learning and developing my skills on my Domain and also in the Software Development Domain.",
+    degree: "Bachelor of Engineering - BE, Computer Science and Engineering with specialisation in CyberSecurity ",
+  },
+  {
+    id: 1,
+    img: "https://i.imgur.com/oUwxYRh.jpeg",
+    school: "Krishna Chaitanya Junior College,Nellore",
+    date: "Jun 2021 - May 2022",
+    grade: "57.0%",
+    desc: "I completed my class 12 high school education at Krishna Chaitanya Junior College,Nellore, where I studied at MPC Group.",
+    degree: "AP State Board (XII), MPC"
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/OMSSU0d.png",
+    school: "Vowel India ,Nellore",
+    date: "Jun 2019 - May 2020",
+    grade: "70.4%",
+    desc: "I completed my class 10 education at Vowel India ,Nellore where I studied at PCM.",
+    degree: "AP State Board (X), PCM",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Mern-Chat",
+    date: "Nov 2023 - Nov 2023",
+    description:
+      "Mern Chat web app which is an Communication Application using the  API.",
+    image:
+      "https://i.imgur.com/yXCcMiI.jpeg",
+    tags: [
+      "HTML",
+      "CSS",
+      "Python",
+      "API",
+      "Flask",
+      "JavaScript",
+    ],
+    category: "web app",
+    github: "https://github.com/Abishek-ak7/ChatApplication/tree/main/chatapp",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "URL Shortener",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Developed a full-stack web application that allows users to short their links using the Long Url.",
+    image:
+      "https://i.imgur.com/Eebi2zf.png",
+    tags: ["HTML","React.js","Node.js","Express.js", "MongoDb", "CSS"],
+    category: "web app",
+    github: "https://github.com/dheeruz/url-shortener",
+    webapp: "https://urlshortner-07-02.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Apple Clone",
+    date: "Oct 2022 - Present",
+    description:
+      "Designed and developed the Web project, a clone of a Apple Products Page.And its my first project .",
+    image:
+      "https://i.imgur.com/fqEtGUN.png",
+    tags: [
+      "HTML",
+      "CSS"
+    ],
+    category: "web app",
+    github: "https://github.com/dheeruz/company_project1",
+    webapp: "https://curious-halva-0c8e9b.netlify.app/",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
